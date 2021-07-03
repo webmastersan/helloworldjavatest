9459382cf6e89bad4386cb8898d37ac5bc9086d2
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-07-03 11:52:52 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World - polling test by jenkins by Puneet");
+		System.out.println("Hello World - polling test by jenkins for star agile");
 	}
 }
